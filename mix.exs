3e057dce6d3be6213bbafb7aaa98b47a5f44b201
Tore Pettersen <toreskog@live.com>
2024-12-19 08:33:42 +0100

    Publish to hex

--- mix.exs
+++ mix.exs
@@ -7,8 +7,14 @@ defmodule Streamline.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
+  end
+
+  defp description do
+    "Streamline pipelines with the ~> operator for handling {:ok, _} and {:error, _} tuples."
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -22,6 +28,15 @@ defmodule Streamline.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: :streamline,
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/torepettersen/streamline"},
+      maintainers: ["Tore Pettersen"]
     ]
   end
 end
